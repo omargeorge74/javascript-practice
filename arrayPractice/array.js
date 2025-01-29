73e1665a0530dc6cf013c3712f6d08295f4f2ae3
Omar George <79220589+omargeorge74@users.noreply.github.com>
2025-01-28 16:05:19 -0800

    new array

--- arrayPractice/array.js
+++ arrayPractice/array.js
@@ -1,2 +1,8 @@
-let sqRoot = Math.sqrt(3);
-console.log(sqRoot);
+const output = document.querySelector(".output");
+
+for (let i = 10; i < 0; i--) {
+  const para = document.createElement("p");
+  output.appendChild(para);
+}
+
+console.log(output);
