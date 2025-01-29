87f2ba28aca6b42b9903dde417c9a7eda76cb0ec
Omar George <79220589+omargeorge74@users.noreply.github.com>
2025-01-28 16:11:18 -0800

    reset

--- arrayPractice/array.js
+++
@@ -1,8 +0,0 @@
-const output = document.querySelector(".output");
-
-for (let i = 10; i < 0; i--) {
-  const para = document.createElement("p");
-  output.appendChild(para);
-}
-
-console.log(output);
