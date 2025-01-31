530ceac418757a6d17d55274b8c1b26dbbc4efa2
Omar George <79220589+omargeorge74@users.noreply.github.com>
2025-01-30 22:03:23 -0800

    finished countdown timer

---
+++ arrayPractice/array.js
@@ -0,0 +1,16 @@
+const output = document.querySelector(".output");
+
+output.textContent = "";
+
+for (let i = 10; i >= 0; i--) {
+  const para = document.createElement("p");
+  if (i === 10) {
+    para.textContent = `Countdown: ${i}`;
+  } else if (i === 0) {
+    para.textContent = `Blast Off!!`;
+  } else {
+    para.textContent = i;
+  }
+  output.appendChild(para);
+  console.log(`${para.textContent} `);
+}
